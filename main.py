edc83eedd517fb8bdbe70451a837618ddf65631b
amir bekhit <bekdevelopment@gmail.com>
2020-11-19 20:37:15 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import json
+import turtle
+import urllib.request
+import time
+import webbrowser
+import geocoder
+
+url = "http://api.open-notify.org/astros.json"
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+file = open("iss.txt", "w")
+file.write("There are currently " +
+           str(result["number"]) + " astronauts on the ISS: \n\n")
+people = result["people"]
+for p in people:
+    file.write(p['name'] + " - on board" + "\n")
+# print long and lat
+g = geocoder.ip('me')
+file.write("\nYour current lat / long is: " + str(g.latlng))
+file.close()
+webbrowser.open("iss.txt")
+
+# Setup the world map in turtle module
+screen = turtle.Screen()
+screen.setup(1280, 720)
+screen.setworldcoordinates(-180, -90, 180, 90)
+
+# load the world map image
+screen.bgpic("map.gif")
+screen.register_shape("iss.gif")
+iss = turtle.Turtle()
+iss.shape("iss.gif")
+iss.setheading(45)
+iss.penup()
+
+while True:
+    # load the current status of the ISS in real-time
+    url = "http://api.open-notify.org/iss-now.json"
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+
+    # Extract the ISS location
+    location = result["iss_position"]
+    lat = location['latitude']
+    lon = location['longitude']
+
+    # Ouput lon and lat to the terminal
+    lat = float(lat)
+    lon = float(lon)
+    print("\nLatitude: " + str(lat))
+    print("\nLongitude: " + str(lon))
+
+    # Update the ISS location on the map
+    iss.goto(lon, lat)
+
+    # Refresh each 5 seconds
+    time.sleep(5)
